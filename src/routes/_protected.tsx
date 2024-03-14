e9dd9760d1fbfee3e1ae187ddbb7ed1df7798c24
agusbw <agusbudhiarta81@gmail.com>
2024-03-14 17:54:17 +0800

    Add token verification in protected route

--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,13 +1,23 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected")({
-  beforeLoad: ({ location, context }) => {
-    if (!context.auth.token)
+  beforeLoad: async ({ location, context }) => {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/verify-token`,
+      {
+        headers: {
+          Authorization: `Bearer ${context.auth.token}`,
+        },
+      }
+    );
+    if (!context.auth.token || !res.ok) {
+      context.auth.logout();
       throw redirect({
         to: "/",
         search: {
           redirect: location.href,
         },
       });
+    }
   },
 });
