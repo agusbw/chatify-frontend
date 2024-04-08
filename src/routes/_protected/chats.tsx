4d221116ebc3bee1702e5b417471627f52d5f918
agusbw <agusbudhiarta81@gmail.com>
2024-04-08 11:25:55 +0800

    fix: add scroll top value just to make sure the scrollTop on its max value

--- src/routes/_protected/chats.tsx
+++ src/routes/_protected/chats.tsx
@@ -74,7 +74,7 @@ function ChatsPage() {
   React.useEffect(() => {
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop =
-        chatContainerRef.current.scrollHeight;
+        chatContainerRef.current.scrollHeight + 300;
     }
   }, [messages]);
 
